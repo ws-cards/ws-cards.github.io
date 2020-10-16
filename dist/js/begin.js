6c95653cdf9073c16b1aa82d4a46f1b6e9127e7c
ws-cards <72481141+ws-cards@users.noreply.github.com>
2020-10-16 12:10:41 +0800

    Update begin.js

--- dist/js/begin.js
+++ dist/js/begin.js
@@ -1,10 +1,10 @@
 window.onload=function(){
 	var isIE = navigator.userAgent.search("Trident") > -1;
 
-    //if (isIE) {
+    if (isIE) {
 		let element = document.getElementById("bodyContent");
 		while (element.firstChild) {
 		  element.removeChild(element.firstChild);
 		}
-	//}
+}
 }
