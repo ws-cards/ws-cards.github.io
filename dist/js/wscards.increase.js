3771e57bf698a4694f2e1270d0bbf6ca0e5bd0b0
ws-cards <72481141+ws-cards@users.noreply.github.com>
2021-08-06 08:36:31 +0800

    Update wscards.increase.js

--- dist/js/wscards.increase.js
+++ dist/js/wscards.increase.js
@@ -58,7 +58,7 @@
 						}else{
 							downArray.push({cardSpread:spread,cardNo:key,price:firstDatePrice});
 							downArray.sort(function(a, b) {
-								return a.cardSpread > b.cardSpread ? 1: -1;
+								return a.cardSpread < b.cardSpread ? 1: -1;
 							});
 							downArray.pop();						
 						}
