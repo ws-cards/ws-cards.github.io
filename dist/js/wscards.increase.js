f32a072e0cf6cc64102d8e68e511bd9bdaa5a418
ws-cards <72481141+ws-cards@users.noreply.github.com>
2021-08-06 09:27:34 +0800

    Update wscards.increase.js

--- dist/js/wscards.increase.js
+++ dist/js/wscards.increase.js
@@ -50,7 +50,7 @@
 							upArray.push({cardSpread:spread,cardNo:key,price:lastDatePrice});
 						}else if("downer" === statute){
 							downArray.push({cardSpread:spread,cardNo:key,price:firstDatePrice});
-							console.log(key+":"+firstDatePrice+":"+spread);
+							console.log("第一次:"+key+":"+firstDatePrice+":"+spread);
 						}	
 					}else{
 						if("upper" === statute){
@@ -59,7 +59,7 @@
 								return a.cardSpread < b.cardSpread ? 1: -1;
 							});
 							upArray.pop();					
-						}else if("downer" === statute){console.log(key+":"+firstDatePrice+":"+spread);
+						}else if("downer" === statute){console.log("連續進入:"+key+":"+firstDatePrice+":"+spread);
 							downArray.push({cardSpread:spread,cardNo:key,price:firstDatePrice});
 							downArray.sort(function(a, b) {
 								return a.cardSpread > b.cardSpread ? 1: -1;
