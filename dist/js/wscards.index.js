1ba453c99c822876f7be83551874d4fa25b93b5d
ws-cards <72481141+ws-cards@users.noreply.github.com>
2022-09-20 17:41:21 +0800

    Add files via upload

--- dist/js/wscards.index.js
+++ dist/js/wscards.index.js
@@ -1,4 +1,4 @@
-			var requestURLGoodsSchedule = 'https://ws-cards.cloud/json/goodsSchedule.json';		
+			var requestURLGoodsSchedule = 'https://ws-cards.cloud/json/friendlyLink.json';		
 			var requestGoodsSchedule = new XMLHttpRequest();	
 			
 			function setFun(){
@@ -6,25 +6,25 @@
 			  requestGoodsSchedule.responseType = 'json';
 			  requestGoodsSchedule.send();	
 			  var scheduleTable = document.getElementById("scheduleTable"); 
-			  var url="https://ws-tcg.com/products/";
 			  requestGoodsSchedule.onload = function(){
 				 var goodsSchedule = requestGoodsSchedule.response;
 				  for(var key in goodsSchedule){
 					  var goods_date = goodsSchedule[key].date;
 					  var goods_title = goodsSchedule[key].title;
+					  var url = goodsSchedule[key].url;
 						var tr = document.createElement("tr");
 						var tdTitle = document.createElement("td");
 						var tdDate = document.createElement("td");
-						tdTitle.innerHTML="<p><a href='"+url+key+"'>"+goods_title+"</a></p>";
-						tdDate.innerHTML="<p>"+goods_date+"</p>";
+						tdTitle.innerHTML="<p><a href='"+url+"'>"+goods_title+"</a></p>";
+						//tdDate.innerHTML="<p>"+goods_date+"</p>";
 						tr.appendChild(tdTitle);
-						tr.appendChild(tdDate);
+						//tr.appendChild(tdDate);
 						scheduleTable.appendChild(tr);
 				  }
 				  			  var table = $('#dataJS').DataTable({
 								"searching" : false,
 								"ordering": false,
-								"pageLength": 5,
+								"pageLength": 10,
 								 "lengthChange": false,
 								  "info": false
 							   });
