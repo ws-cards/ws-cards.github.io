852389c03fa22d68dc1122f54c5a67e43e29fac2
ws-cards <72481141+ws-cards@users.noreply.github.com>
2025-06-14 03:20:09 +0800

    Update wscards.line.js

--- dist/js/wscards.line.js
+++ dist/js/wscards.line.js
@@ -170,7 +170,8 @@ source:[
 {id:"WS00149", name:"ガールズバンドクライ",cname:"Girls Band Cry 少女樂團 吶喊吧 少哭 少女樂團哭 | GCR"},
 {id:"WS00150", name:"ラブライブ！蓮ノ空女学院スクールアイドルクラブ feat. Link！Like！ラブライブ！",cname:"蓮之空女學院學園偶像俱樂部 | LHS"},
 {id:"WS00151", name:"甘神さんちの縁結び",cname:"結緣甘神神社 | AMG"},
-{id:"WS00152", name:"学園アイドルマスター",cname:"學園偶像大師 | 學偶 | GIM"}
+{id:"WS00152", name:"学園アイドルマスター",cname:"學園偶像大師 | 學偶 | GIM"},
+{id:"WS00153", name:"あおぎり高校",cname:"青桐高中 | AOH"}
 ]
 ,
   minLength:1,
